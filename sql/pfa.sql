56e583f1b97223f6a52a01d40622ea6f578b2803
hai <xmhai@hotmail.com>
2020-05-19 13:04:27 +0800

    Update pfa.sql

--- sql/pfa.sql
+++ sql/pfa.sql
@@ -1,8 +1,8 @@
 -- PFA database design
 
 -- Code Table
-DROP TABLE IF EXISTS cfg_code_type;
-CREATE TABLE cfg_code_type (
+DROP TABLE IF EXISTS code_type;
+CREATE TABLE code_type (
   id int NOT NULL AUTO_INCREMENT,
   code_type_name varchar(60) NOT NULL,
   code_type_desc varchar(100),
@@ -14,8 +14,8 @@ CREATE TABLE cfg_code_type (
   PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS cfg_code_table;
-CREATE TABLE cfg_code_table (
+DROP TABLE IF EXISTS code_table;
+CREATE TABLE code_table (
   id int NOT NULL AUTO_INCREMENT,
   code_type_id varchar(20) NOT NULL,
   code_name varchar(20) NOT NULL,
@@ -29,13 +29,13 @@ CREATE TABLE cfg_code_table (
   PRIMARY KEY (id)
 );
 -- insert testing data
-INSERT INTO cfg_code_type (id, code_type_name, created_by, created_date) VALUES(1, 'Transaction Type', 0, CURRENT_DATE);
-INSERT INTO cfg_code_table (code_type_id, code_name, code_value, created_by, created_date) VALUES(1, 'BUY', 'Buy', 0, CURRENT_DATE);
-INSERT INTO cfg_code_table (code_type_id, code_name, code_value, created_by, created_date) VALUES(1, 'SELL', 'Sell', 0, CURRENT_DATE);
+INSERT INTO code_type (id, code_type_name, created_by, created_date) VALUES(1, 'Transaction Type', 0, CURRENT_DATE);
+INSERT INTO code_table (code_type_id, code_name, code_value, created_by, created_date) VALUES(1, 'BUY', 'Buy', 0, CURRENT_DATE);
+INSERT INTO code_table (code_type_id, code_name, code_value, created_by, created_date) VALUES(1, 'SELL', 'Sell', 0, CURRENT_DATE);
 
 -- Asset category configuration table
-DROP TABLE IF EXISTS cfg_category;
-CREATE TABLE cfg_category (
+DROP TABLE IF EXISTS asset_category;
+CREATE TABLE asset_category (
   id int NOT NULL AUTO_INCREMENT,
   code varchar(20) NOT NULL,
   created_by int NOT NULL,
@@ -46,17 +46,17 @@ CREATE TABLE cfg_category (
   PRIMARY KEY (id)
 );
 
-INSERT INTO cfg_category (id, code, created_by, created_date) VALUES(1, 'STOCK', 0, CURRENT_DATE);
-INSERT INTO cfg_category (id, code, created_by, created_date) VALUES(2, 'REIT', 0, CURRENT_DATE);
-INSERT INTO cfg_category (id, code, created_by, created_date) VALUES(3, 'BOND', 0, CURRENT_DATE);
-INSERT INTO cfg_category (id, code, created_by, created_date) VALUES(4, 'CASH', 0, CURRENT_DATE);
-INSERT INTO cfg_category (id, code, created_by, created_date) VALUES(5, 'CPF_ORDINARY', 0, CURRENT_DATE);
-INSERT INTO cfg_category (id, code, created_by, created_date) VALUES(6, 'CPF_MEDISAVE', 0, CURRENT_DATE);
-INSERT INTO cfg_category (id, code, created_by, created_date) VALUES(7, 'CPF_SPECIAL', 0, CURRENT_DATE);
+INSERT INTO asset_category (id, code, created_by, created_date) VALUES(1, 'STOCK', 0, CURRENT_DATE);
+INSERT INTO asset_category (id, code, created_by, created_date) VALUES(2, 'REIT', 0, CURRENT_DATE);
+INSERT INTO asset_category (id, code, created_by, created_date) VALUES(3, 'BOND', 0, CURRENT_DATE);
+INSERT INTO asset_category (id, code, created_by, created_date) VALUES(4, 'CASH', 0, CURRENT_DATE);
+INSERT INTO asset_category (id, code, created_by, created_date) VALUES(5, 'CPF_ORDINARY', 0, CURRENT_DATE);
+INSERT INTO asset_category (id, code, created_by, created_date) VALUES(6, 'CPF_MEDISAVE', 0, CURRENT_DATE);
+INSERT INTO asset_category (id, code, created_by, created_date) VALUES(7, 'CPF_SPECIAL', 0, CURRENT_DATE);
 
 -- Exchange configuration table
-DROP TABLE IF EXISTS cfg_exchange;
-CREATE TABLE cfg_exchange (
+DROP TABLE IF EXISTS exchange;
+CREATE TABLE exchange (
   id int NOT NULL AUTO_INCREMENT,
   code varchar(10) NOT NULL,
   created_by int NOT NULL,
@@ -67,15 +67,15 @@ CREATE TABLE cfg_exchange (
   PRIMARY KEY (id)
 );
 
-INSERT INTO cfg_exchange (id, code, created_by, created_date) VALUES(1, 'SGX', 0, CURRENT_DATE);
-INSERT INTO cfg_exchange (id, code, created_by, created_date) VALUES(2, 'CN', 0, CURRENT_DATE);
-INSERT INTO cfg_exchange (id, code, created_by, created_date) VALUES(3, 'NYSE', 0, CURRENT_DATE);
-INSERT INTO cfg_exchange (id, code, created_by, created_date) VALUES(4, 'HKEX', 0, CURRENT_DATE);
-INSERT INTO cfg_exchange (id, code, created_by, created_date) VALUES(5, 'NASDAQ', 0, CURRENT_DATE);
+INSERT INTO exchange (id, code, created_by, created_date) VALUES(1, 'SGX', 0, CURRENT_DATE);
+INSERT INTO exchange (id, code, created_by, created_date) VALUES(2, 'CN', 0, CURRENT_DATE);
+INSERT INTO exchange (id, code, created_by, created_date) VALUES(3, 'NYSE', 0, CURRENT_DATE);
+INSERT INTO exchange (id, code, created_by, created_date) VALUES(4, 'HKEX', 0, CURRENT_DATE);
+INSERT INTO exchange (id, code, created_by, created_date) VALUES(5, 'NASDAQ', 0, CURRENT_DATE);
 
 -- Currency configuration table
-DROP TABLE IF EXISTS cfg_currency;
-CREATE TABLE cfg_currency (
+DROP TABLE IF EXISTS currency;
+CREATE TABLE currency (
   id int NOT NULL AUTO_INCREMENT,
   code varchar(10) NOT NULL,
   exchange_rate decimal(10,2) NOT NULL, -- yesterday exchange rate updated by scheduled job
@@ -87,13 +87,13 @@ CREATE TABLE cfg_currency (
   PRIMARY KEY (id)
 );
 
-INSERT INTO cfg_currency (id, code, exchange_rate, created_by, created_date) VALUES(1, 'SGD', 1, 0, CURRENT_DATE);
-INSERT INTO cfg_currency (id, code, exchange_rate, created_by, created_date) VALUES(2, 'RMB', 0.2, 0, CURRENT_DATE);
-INSERT INTO cfg_currency (id, code, exchange_rate, created_by, created_date) VALUES(3, 'USD', 1.4, 0, CURRENT_DATE);
-INSERT INTO cfg_currency (id, code, exchange_rate, created_by, created_date) VALUES(4, 'HKD', 0.18, 0, CURRENT_DATE);
+INSERT INTO currency (id, code, exchange_rate, created_by, created_date) VALUES(1, 'SGD', 1, 0, CURRENT_DATE);
+INSERT INTO currency (id, code, exchange_rate, created_by, created_date) VALUES(2, 'RMB', 0.2, 0, CURRENT_DATE);
+INSERT INTO currency (id, code, exchange_rate, created_by, created_date) VALUES(3, 'USD', 1.4, 0, CURRENT_DATE);
+INSERT INTO currency (id, code, exchange_rate, created_by, created_date) VALUES(4, 'HKD', 0.18, 0, CURRENT_DATE);
 
-DROP TABLE IF EXISTS cfg_transaction_type;
-CREATE TABLE cfg_transaction_type (
+DROP TABLE IF EXISTS transaction_type;
+CREATE TABLE transaction_type (
   id int NOT NULL AUTO_INCREMENT,
   code varchar(10) NOT NULL,
   created_by int NOT NULL,
@@ -104,8 +104,8 @@ CREATE TABLE cfg_transaction_type (
   PRIMARY KEY (id)
 );
 
-INSERT INTO cfg_transaction_type (id, code, created_by, created_date) VALUES(1, 'BUY', 0, CURRENT_DATE);
-INSERT INTO cfg_transaction_type (id, code, created_by, created_date) VALUES(2, 'SELL', 0, CURRENT_DATE);
+INSERT INTO transaction_type (id, code, created_by, created_date) VALUES(1, 'BUY', 0, CURRENT_DATE);
+INSERT INTO transaction_type (id, code, created_by, created_date) VALUES(2, 'SELL', 0, CURRENT_DATE);
 
 -- Asset Account (all asset must be in this table)
 DROP TABLE IF EXISTS account;

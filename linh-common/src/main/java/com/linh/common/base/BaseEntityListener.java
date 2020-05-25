1adeeb818bc9244d5615688ab8590f872ae55f6c
hai <xmhai@hotmail.com>
2020-05-25 23:01:29 +0800

    Updated linh-common

--- linh-common/src/main/java/com/linh/common/base/BaseEntityListener.java
+++ linh-common/src/main/java/com/linh/common/base/BaseEntityListener.java
@@ -8,12 +8,14 @@ import javax.persistence.PreUpdate;
 public class BaseEntityListener {
     @PrePersist
     public void setCreated(BaseEntity entity) {
+    	entity.setCreatedBy(0L);
     	entity.setCreatedDate(LocalDateTime.now());
     	entity.setIsDeleted(false);
     }
     
     @PreUpdate
     public void setUpdated(BaseEntity entity) {
+    	entity.setUpdatedBy(0L);
     	entity.setUpdatedDate(LocalDateTime.now());
     }
 }
--- linh-common/src/main/java/com/linh/common/base/BusinessException.java
+++ linh-common/src/main/java/com/linh/common/base/BusinessException.java
@@ -1,5 +1,7 @@
 package com.linh.common.base;
 
 public class BusinessException extends Exception {
-
+    public BusinessException(String message) {
+        super(message);
+    }
 }

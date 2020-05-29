a19838fd6acfec437ced87d673249265a1bf99a1
hai <xmhai@hotmail.com>
2020-05-29 14:41:38 +0800

    Genereate AUTO_INCREMENT id column

--- linh-common/src/main/java/com/linh/common/base/BaseEntity.java
+++ linh-common/src/main/java/com/linh/common/base/BaseEntity.java
@@ -28,7 +28,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class BaseEntity {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@JsonIgnore

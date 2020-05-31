a6f1663ceec4c2b19c406bd35edb27144a591c96
hai <xmhai@hotmail.com>
2020-05-31 08:30:53 +0800

    Fixed undefined display issue

--- frontend/src/shared/components/index.js
+++ frontend/src/shared/components/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
 export const ColorNumber = ({ value }) => {
-    return (
-        <div style={{ color: value > 0 ? "green" : "red", textAlign: "right" } }>{value.toLocaleString('en-US', { minimumFractionDigits: 2 })} </div>
-    );
+    if (value || value === 0) {
+        return (
+            <div style={{ color: value >= 0 ? "green" : "red", textAlign: "right" } }>{value.toLocaleString('en-US', { minimumFractionDigits: 2 })} </div>
+        );
+    }
+
+    // undefine
+    return <div />
 };
 
 export const Number = ({ value }) => {
-    return (
-        <div>{value.toLocaleString('en-US', { minimumFractionDigits: 2 })} </div>
-    );
+    if (value || value === 0) {
+        return (
+            <div>{value.toLocaleString('en-US', { minimumFractionDigits: 2 })} </div>
+        );
+    }
+
+    return <div />
 };

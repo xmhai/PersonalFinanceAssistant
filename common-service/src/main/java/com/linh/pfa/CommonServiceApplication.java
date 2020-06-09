88c76b5a27e099f39b6d4fcbada30547a5013f8b
hai <xmhai@hotmail.com>
2020-06-09 23:33:16 +0800

    Wait mysql to startup before other services

--- common-service/src/main/java/com/linh/pfa/CommonServiceApplication.java
+++ common-service/src/main/java/com/linh/pfa/CommonServiceApplication.java
@@ -1,33 +1,15 @@
 package com.linh.pfa;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.linh")
 public class CommonServiceApplication {
-
-    @Value("${env.var}")
-    private String envVar;
-
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return new CommandLineRunner() {
-            @Override
-            public void run(String[] arg0) throws Exception {
-                System.out.println(envVar);
-            }
-        };
-    }
-    
 	public static void main(String[] args) {
 		SpringApplication.run(CommonServiceApplication.class, args);
 	}

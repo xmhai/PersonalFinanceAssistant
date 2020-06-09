cc5f3aefe7c76f5e8b266ebdd8c40d1b803fe714
hai <xmhai@hotmail.com>
2020-06-09 18:58:35 +0800

    Docker compose

--- common-service/src/main/java/com/linh/pfa/CommonServiceApplication.java
+++ common-service/src/main/java/com/linh/pfa/CommonServiceApplication.java
@@ -1,5 +1,7 @@
 package com.linh.pfa;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -13,6 +15,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @ComponentScan(basePackages = "com.linh")
 public class CommonServiceApplication {
 
+    @Value("${env.var}")
+    private String envVar;
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String[] arg0) throws Exception {
+                System.out.println(envVar);
+            }
+        };
+    }
+    
 	public static void main(String[] args) {
 		SpringApplication.run(CommonServiceApplication.class, args);
 	}

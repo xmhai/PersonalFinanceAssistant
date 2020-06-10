b87d5143870ff6e31233e8ccf4560dbb83003970
hai <xmhai@hotmail.com>
2020-06-10 21:35:36 +0800

    Job service intialize its own data

--- common-service/src/main/resources/db/changelog/init-data.sql
+++ common-service/src/main/resources/db/changelog/init-data.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
---changeset hai:1
+--changeset common-hai:1
 -- asset_category
 INSERT INTO asset_category (id, code, created_by, created_date, is_deleted) VALUES(1, 'STOCK', 0, CURRENT_DATE, 0);
 INSERT INTO asset_category (id, code, created_by, created_date, is_deleted) VALUES(2, 'REIT', 0, CURRENT_DATE, 0);
@@ -22,6 +22,3 @@ INSERT INTO currency (id, code, exchange_rate, created_by, created_date, is_dele
 -- action
 INSERT INTO action (id, code, created_by, created_date, is_deleted) VALUES(1, 'BUY', 0, CURRENT_DATE, 0);
 INSERT INTO action (id, code, created_by, created_date, is_deleted) VALUES(2, 'SELL', 0, CURRENT_DATE, 0);
-
-INSERT INTO job_config (id,created_by,created_date,is_deleted,updated_by,updated_date,cron_expression,description,end_time,job_class_name,name,start_time) 
-     VALUES (1,0,'2020-06-02 00:00:00',0,0,'2020-06-06 08:32:47','0 0 18 ? * MON-FRI','Update Stock Price Every day on 6pm','2022-07-31 16:00:00','com.linh.pfa.job.stock.PriceUpdateJob','updateprice','2020-06-01 00:00:00');

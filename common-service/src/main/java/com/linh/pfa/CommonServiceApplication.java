0069d8b2301208440c0746f7ea002cb457ca3184
xmhai <xmhai@hotmail.com>
2020-12-30 20:27:41 +0800

    Enable CORS for common service for dev

--- common-service/src/main/java/com/linh/pfa/CommonServiceApplication.java
+++ common-service/src/main/java/com/linh/pfa/CommonServiceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,6 +18,19 @@ public class CommonServiceApplication {
 	public static void main(String[] args) {
 		log.info("Application running...");
 		SpringApplication.run(CommonServiceApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+					.allowedOrigins("*")
+					.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+					.allowCredentials(true);
+			}
+		};
 	}
 
 	@Bean

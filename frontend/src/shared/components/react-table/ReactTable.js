271adfc458a7ea4dbfaff6e92ddaf08ac82c5227
xmhai <xmhai@hotmail.com>
2021-01-15 19:32:56 +0800

    Fixed Pagination issues: fiter count & position

--- frontend/src/shared/components/react-table/ReactTable.js
+++ frontend/src/shared/components/react-table/ReactTable.js
@@ -21,6 +21,70 @@ import {
 
 import ReactTableToolbar from './ReactTableToolbar'
 
+import IconButton from '@material-ui/core/IconButton';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
+
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import DescriptionIcon from '@material-ui/icons/Description';
+
+import history from '../../../app/components/history';
+
+const useStyles1 = makeStyles((theme) => ({
+  root: {
+    flexShrink: 0,
+    marginLeft: theme.spacing(2.5),
+  },
+}));
+
+function TablePaginationActions(props) {
+  const classes = useStyles1();
+  const { count, page, rowsPerPage, onChangePage } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onChangePage(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onChangePage(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onChangePage(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <div>
+      <IconButton onClick={handleFirstPageButtonClick} disabled={page === 0} aria-label="first page" >
+        <FirstPageIcon />
+      </IconButton>
+      <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page" >
+        <KeyboardArrowLeft />
+      </IconButton>
+      <IconButton onClick={handleNextButtonClick} disabled={page >= Math.ceil(count / rowsPerPage) - 1} aria-label="next page" >
+        <KeyboardArrowRight />
+      </IconButton>
+      <IconButton onClick={handleLastPageButtonClick} disabled={page >= Math.ceil(count / rowsPerPage) - 1} aria-label="last page" >
+        <LastPageIcon />
+      </IconButton>
+    </div>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onChangePage: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
+
 const useStyles = makeStyles({
   table: {
     fontSize: "20px"
@@ -34,42 +98,64 @@ const useStyles = makeStyles({
     padding: "14px 5px",
     fontSize: "14px"
   },
-  body: {
-    minWidth: 650,
-  },
   footer: {
     padding: "14px 5px",
     fontSize: "14px"
   },
+  spacer: {
+    flex: '0 0 auto',
+  }
 });
 
 export const ReactTable = ({
-  title,
   columns,
   data,
-  skipPageReset,
+  baseUrl,
+  title,
+  showAddButton,
+  showViewButton,
+  showEditButton,
+  showDeleteButton,
 }) => {
   const {
     getTableProps,
+    getTableBodyProps,
     headerGroups,
     footerGroups,
+    rows,
     prepareRow,
-    page,
-    gotoPage,
-    setPageSize,
     preGlobalFilteredRows,
     setGlobalFilter,
+    state,
+
+    page, // Instead of using 'rows', we'll use page,
+    // which has only the rows for the active page
+
+    // The rest of these things are super handy, too ;)
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+
     state: { pageIndex, pageSize, globalFilter },
   } = useTable(
     {
       columns,
-      data,
-      autoResetPage: !skipPageReset,
+      data
     },
     useGlobalFilter,
     useSortBy,
     usePagination,
   )
+
+  React.useEffect(() => {
+    // props.dispatch({ type: actions.resetPage })
+    console.log(globalFilter);
+  }, [globalFilter]);
 
   const handleChangePage = (event, newPage) => {
     gotoPage(newPage)
@@ -78,6 +164,21 @@ export const ReactTable = ({
   const handleChangeRowsPerPage = event => {
     setPageSize(Number(event.target.value))
   }
+
+  const handleView = row => {
+    history.push(`${baseUrl}/${row.original.id}`);
+  }
+
+  const handleEdit = row => {
+    history.push(`${baseUrl}/edit/${row.original.id}`);
+  }
+
+  const handleDelete = row => {
+    history.push(`${baseUrl}/delete/${row.original.id}`);
+  }
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows = pageIndex > 0 ? Math.max(0, (1 + pageIndex) * pageSize - rows.length) : 0;
 
   const classes = useStyles();
 
@@ -89,6 +190,7 @@ export const ReactTable = ({
         preGlobalFilteredRows={preGlobalFilteredRows}
         setGlobalFilter={setGlobalFilter}
         globalFilter={globalFilter}
+        baseUrl={baseUrl}
       />        
       <Table {...getTableProps()} className={classes.table}>
         <TableHead>
@@ -115,6 +217,7 @@ export const ReactTable = ({
                     />) : null}
                 </TableCell>
               ))}
+              <TableCell className={classes.headerCell} align="center">Action</TableCell>
             </TableRow>
           ))}
         </TableHead>
@@ -130,9 +233,29 @@ export const ReactTable = ({
                     </TableCell>
                   )
                 })}
+                <TableCell className={classes.cell} align="center">
+                  <div>
+                      <IconButton aria-label="view" onClick={() => handleView(row)}>
+                          <DescriptionIcon />
+                      </IconButton>
+                      <IconButton aria-label="edit" onClick={() => handleEdit(row)}>
+                          <EditIcon />
+                      </IconButton>
+                      <IconButton aria-label="delete" onClick={() => handleDelete(row)}>
+                          <DeleteIcon />
+                      </IconButton>
+                  </div>
+                </TableCell>
               </TableRow>
             )
           })}
+
+          {emptyRows > 0 && (
+            <TableRow style={{ height: 47.2 * emptyRows }}>
+              <TableCell />
+            </TableRow>
+          )}
+
         </TableBody>
 
         <TableFooter>
@@ -141,18 +264,17 @@ export const ReactTable = ({
                 {group.headers.map(column => (
                 <TableCell align='right' {...column.getFooterProps()} className={classes.footer}>{column.render('Footer')}</TableCell>
                 ))}
+                <TableCell> </TableCell>
             </TableRow>
           ))}            
           <TableRow>
             <TablePagination
-              rowsPerPageOptions={[
-                5,
-                10,
-                25,
-                { label: 'All', value: data.length },
-              ]}
-              colSpan={3}
-              count={data.length}
+              classes={{
+                spacer: classes.spacer,
+              }}
+              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: data.length }, ]}
+              colSpan={columns.length + 1}
+              count={rows.length}
               rowsPerPage={pageSize}
               page={pageIndex}
               SelectProps={{
@@ -161,6 +283,8 @@ export const ReactTable = ({
               }}
               onChangePage={handleChangePage}
               onChangeRowsPerPage={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+              align="left"
             />
           </TableRow>
         </TableFooter>

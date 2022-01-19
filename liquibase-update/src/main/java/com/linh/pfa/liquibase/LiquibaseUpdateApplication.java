83ba31d74daf8e7f9b5b4768984f4acf6321f241
xmhai <xmhai@hotmail.com>
2022-01-19 10:17:04 +0800

    Move liquibase to seperate project

---
+++ liquibase-update/src/main/java/com/linh/pfa/liquibase/LiquibaseUpdateApplication.java
@@ -0,0 +1,13 @@
+package com.linh.pfa.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseUpdateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseUpdateApplication.class, args);
+	}
+
+}

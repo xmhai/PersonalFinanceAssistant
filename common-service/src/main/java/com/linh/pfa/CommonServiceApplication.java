26a9f81b26684dea7af709e9d4273adfd7e4d10b
xmhai <xmhai@hotmail.com>
2022-01-23 13:13:06 +0800

    common-service in k8s

--- common-service/src/main/java/com/linh/pfa/CommonServiceApplication.java
+++ common-service/src/main/java/com/linh/pfa/CommonServiceApplication.java
@@ -2,12 +2,7 @@ package com.linh.pfa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.extern.slf4j.Slf4j;
 

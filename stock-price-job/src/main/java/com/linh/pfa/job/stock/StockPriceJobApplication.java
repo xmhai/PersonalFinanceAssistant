edd64b52e45f62c0cbadc2ccf5c8022fad078f64
xmhai <xmhai@hotmail.com>
2022-03-13 23:25:01 +0800

    Stop application after batch processing

--- stock-price-job/src/main/java/com/linh/pfa/job/stock/StockPriceJobApplication.java
+++ stock-price-job/src/main/java/com/linh/pfa/job/stock/StockPriceJobApplication.java
@@ -27,7 +27,7 @@ import org.springframework.web.client.RestTemplate;
 public class StockPriceJobApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StockPriceJobApplication.class, args);
+		SpringApplication.exit(SpringApplication.run(StockPriceJobApplication.class, args));
 	}
 
 	@Bean
